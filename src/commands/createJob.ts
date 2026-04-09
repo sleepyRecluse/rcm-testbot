85f4b0e2794341718c5702504d0bf5990fd423b2
Juan <juan03josev@gmail.com>
2026-04-08 22:02:52 -0500

    The createJob command now requires two values, job id and job name. Creates channels combining the two restricting view and message access to only user using bot. Updated json packages.

--- src/commands/createJob.ts
+++ src/commands/createJob.ts
@@ -2,35 +2,57 @@ import {
   ChannelType,
   ChatInputCommandInteraction,
   SlashCommandBuilder,
+  PermissionFlagsBits,
 } from "discord.js";
 
 export const data = new SlashCommandBuilder()
   .setName("create-job")
   .setDescription("Creates new category using user selected name")
   .addStringOption((option) =>
-    option
-      .setName("name")
-      .setDescription("Create job channel")
-      .setRequired(true),
+    option.setName("job-id").setDescription("Job Id").setRequired(true),
+  )
+  .addStringOption((option) =>
+    option.setName("job-name").setDescription("Job Name").setRequired(true),
   );
 
 export async function execute(interaction: ChatInputCommandInteraction) {
-  const jobName = interaction.options.getString("name", true);
+  const jobId = interaction.options.getString("job-id", true);
+  const jobName = interaction.options.getString("job-name", true);
   const guild = interaction.guild;
+  const roleId = interaction.user.id;
+  console.log(roleId);
 
   if (guild) {
-    const category = await guild.channels.create({
-      name: `${jobName}`,
-      type: ChannelType.GuildCategory,
-    });
+    // const category = await guild.channels.create({
+    //   name: `${jobName}`,
+    //   type: ChannelType.GuildCategory,
+    // });
+
     const channel = await guild.channels.create({
-      name: `${jobName}_Channel`,
+      name: `${jobId}_${jobName}`,
       type: ChannelType.GuildForum,
-      parent: category,
+      parent: "1491611687478890596",
+
+      permissionOverwrites: [
+        {
+          id: guild.roles.everyone.id,
+          deny: [
+            PermissionFlagsBits.ViewChannel,
+            PermissionFlagsBits.SendMessages,
+            PermissionFlagsBits.SendMessagesInThreads,
+          ],
+        },
+        {
+          id: roleId,
+          allow: [
+            PermissionFlagsBits.ViewChannel,
+            PermissionFlagsBits.SendMessages,
+            PermissionFlagsBits.SendMessagesInThreads,
+          ],
+        },
+      ],
     });
 
-    await interaction.reply(
-      `Created category ${category} and Created channel: ${channel}`,
-    );
+    await interaction.reply(`Created channel: ${channel}`);
   }
 }
